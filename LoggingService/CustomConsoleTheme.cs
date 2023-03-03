68f52080fb9582011ff69a0d730a0fb9ef8a1bf2
ahmed elrakhawy <elrakhawyahmed79@gmail.com>
2023-03-03 13:46:01 +0200

    Creating the project , intailizing dbcontext and some entities

---
+++ LoggingService/CustomConsoleTheme.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace LoggingService
+{
+    public class CustomConsoleTheme
+    {
+        private readonly IReadOnlyDictionary<ConsoleThemeStyle, string> _styles;
+
+        public CustomConsoleTheme(IReadOnlyDictionary<ConsoleThemeStyle, string> styles)
+        {
+            if (styles == null)
+            {
+                throw new ArgumentNullException(nameof(styles));
+            }
+
+            this._styles = styles.ToDictionary(kv => kv.Key, kv => kv.Value);
+        }
+
+        public static CustomConsoleTheme VisualStudioLight { get; } = CustomConsoleThemes.VisualStudioLight;
+
+    }
+}
